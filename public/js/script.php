cb053a2c6b350f00bdf4ab0f5ac06865e5bf7a20
ErikMartrus <erik_mg94@hotmail.es>
2019-10-23 13:38:02 +0100

    Solucionado el botón de AddCanal y trabajando con el script

---
+++ public/js/script.php
@@ -0,0 +1,28 @@
+<?php
+$servername = "mysql.hostinger.co.uk";
+$database = "u266072517_name";
+$email = "u266072517_email";
+$password = "buystuffpwd";
+$fecha_de_nacimiento ="u266072517_fechaNacimiento";
+$fecha= date("Y-m-d H:i:s");
+// Create connection
+
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+// Check connection
+
+if (!$conn) {
+      die("Connection failed: " . mysqli_connect_error());
+}
+ 
+echo "Connected successfully";
+ 
+$sql = "INSERT INTO users (name, email, password,fecha_de_nacimiento,fecha) VALUES ('Thom', 'Vial', 'thom.v@some.com')";
+if (mysqli_query($conn, $sql)) {
+      echo "New record created successfully";
+} else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+mysqli_close($conn);
+
+?>
