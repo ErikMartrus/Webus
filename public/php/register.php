3be7aee69d47a666c79b7d1b26391a81a1c28199
ErikMartrus <erik_mg94@hotmail.es>
2019-10-30 13:17:53 +0000

    Añadiendo los php

---
+++ public/php/register.php
@@ -0,0 +1,34 @@
+<?php
+$name = $_POST["name"];
+$fechaDeNacimiento = $_POST["fechaDeNacimiento"];
+$email = $_POST["email"];
+$pass = $_POST["pass"];
+$fecha= date("Y-m-d H:i:s");
+// Create connection
+
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+// Check connection
+
+if (!$conn) {
+      die("Connection failed: " . mysqli_connect_error());
+}
+ 
+echo "Connected successfully";
+
+$insertar = "INSERT INTO users(name,fechaDeNacimiento,email, pass,fecha) VALUES ('$nombre', '$fechaDeNacimiento', '$email', '$pass', '$fecha')";
+//EJECUTAR CONSULTA:
+//La variable conexion tiene almacenado los datos para acceder a la base de datos (servidor, usuario, contraseña y nombre de la bd).
+
+$resultado = mysqli_query($conexion, $insertar);
+
+if (!$resultado) {
+    echo 'Error al registrarse';
+} else {
+    echo 'Usuario registrado exitosamente';
+}
+
+//CERRAR CONEXION
+mysqli_close($conexion);
+    
+?>
---
+++ public/php/update.php
@@ -0,0 +1,36 @@
+<?php
+
+function saveInformationToDatabase($url,$dato){
+//cojo los datos del campo usuairo y los almaceno en la variable $usuario
+$dato = $_POST['dato'];
+$url = $_POST['url'];
+$fecha= date("Y-m-d H:i:s");
+//conectar a la base de datos
+//la url es de la tabla canales id canal de tabla sensores
+$conexion = mysqli_connect("localhost", "root", "root", "bd_prueba");
+//voy a seleccionar el campo id canal en la base de datos que sea igual al dato que he ingresado. El dato que he ingresado tiene que coincidir con el de la base de datos
+$consulta = "SELECT * FROM canales WHERE url='$url'";
+$resultado=mysqli_query($conexion, $consulta);
+//si ha conincidido me da un resultado y sino me da cero
+$idToSave=0;
+$filas=mysqli_num_rows($resultado);
+if ($row=mysqli_fetch_assoc($result)) {
+    $idToSave=$row("id");
+
+}
+
+$insertar = "INSERT INTO datossensores(idcanal,dato,fecha) VALUES ('$idToSave','$dato','$fecha')";
+
+//liberar los resultados, para que no consuma espacio en memoria
+mysqli_free_result($resultado);
+mysqli_close($conexion);
+
+if($_SERVER["REQUEST_METHOD"]=="POST"){
+    if(isset($_POST["url"])&& isset($_POST["dato"])){
+        $urlCompartir=$_POST["url"];
+        $datoCompartir=$_POST["dato"];
+        saveInformationToDatabase($urlCompartir, $datoCompartir)
+    }
+}
+
+?>
