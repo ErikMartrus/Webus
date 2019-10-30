7092702b8f1c0846fdf2cff898f8d16d7fb38648
ErikMartrus <erik_mg94@hotmail.es>
2019-10-30 13:52:07 +0000

    CambiandoIds

--- public/php/altaCanal.php
+++ public/php/altaCanal.php
@@ -1,4 +1,15 @@
 <?php
+$conexion = mysqli_connect("localhost", "root", "root", "bd_prueba");
+//la variable nombre va a recibir los datos del campo que se llama nombre, a traves del metodo POST
+//RECIBIR LO DATOS Y ALMACERNALOS EN VARIABLES:
+$nombre = $_POST["nombre"];
+$description = $_POST["description"];
+$longitud = $_POST["longitud"];
+$latitud = $_POST["latitud"];
+$nombreDelSensor = $_POST["nombreDelSensor"];
+
+
+
 
 
 ?>
