5e92eb5eac513661441032c05233d1a7869cdb57
ErikMartrus <erik_mg94@hotmail.es>
2019-11-06 12:14:40 +0000

    Se añade imagen nueva y cambios minimos

--- public/php/update.php
+++ public/php/update.php
@@ -5,7 +5,7 @@ function saveInformationToDatabase($url,$dato){
 $fecha= date("Y-m-d H:i:s");
 //conectar a la base de datos
 //la url es de la tabla canales id canal de tabla sensores
-$conexion = mysqli_connect("localhost", "root", "root", "bd_prueba");
+$conexion = mysqli_connect("localhost", "root", "", "bd_prueba");
 //voy a seleccionar el campo id canal en la base de datos que sea igual al dato que he ingresado. El dato que he ingresado tiene que coincidir con el de la base de datos
 $consulta = "SELECT * FROM canales WHERE url='$url'";
 $resultado=mysqli_query($conexion, $consulta);
