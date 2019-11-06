1c4cddb7da598103592513543fb819698f93c56f
ErikMartrus <erik_mg94@hotmail.es>
2019-11-06 12:50:24 +0000

    Solucionados problemas para añadir canales

--- public/php/altaCanal.php
+++ public/php/altaCanal.php
@@ -23,7 +23,7 @@ $nombreDelSensor = $_POST["nombreDelSensor"];
 $url= rand() . "\n"; //n significa new line(salto de línea) y el punto =+
 
 //saber id del usuario
-$insertar = "INSERT INTO canales(nombre,descripcion,longitud,latitud,nombreDelSensor,url) VALUES ('$name','$descripcion','$longitud','$latitud','$nombreDelSensor','$url')";
+$insertar = "INSERT INTO canales(nombreCanal,descripcion,longitud,latitud,nombreSensor,url) VALUES ('$name','$descripcion','$longitud','$latitud','$nombreDelSensor','$url')";
 
 
 //EJECUTAR CONSULTA:
