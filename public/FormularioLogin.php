fac560f0f5484bfac89e3741a3acdbb5202889de
ErikMartrus <erik_mg94@hotmail.es>
2019-11-13 00:53:40 +0000

    Cambios

--- public/FormularioLogin.php
+++ public/FormularioLogin.php
@@ -54,7 +54,7 @@
         ?>
         <ul class="navbar-nav">
           <li class="nav-item active">
-            <a class="nav-link" href="login.php">Login<span class="sr-only">(current)</span></a>
+            <a class="nav-link" href="FormularioLogin.php">Login<span class="sr-only">(current)</span></a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="register.php">Register</a>
