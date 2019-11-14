4484d9a174c8240ba78956813702fb9be8daff1b
ErikMartrus <erik_mg94@hotmail.es>
2019-11-14 12:33:02 +0000

    creacion ultimosCanales.php

---
+++ public/php/ultimosCanales.php
@@ -0,0 +1,4 @@
+<script src="./grafica/highcharts.js"></script>
+<script src="./grafica/modules/series-label.js"></script>
+<script src="./grafica/modules/exporting.js"></script>
+<script src="./grafica/modules/export-data.js"></script>
