7bf38f1609061b61c5a9cf6378ea098242186c9a
ErikMartrus <erik_mg94@hotmail.es>
2019-11-16 11:42:01 +0000

    update.php

--- public/php/update.php
+++ public/php/update.php
@@ -1,31 +1,31 @@
 <?php
     if($_SERVER['REQUEST_METHOD']=="POST"){
-        if(isset($_POST['dato'])&&isset($_POST['url'])){
+        if(isset($_POST['dato'])&& isset($_POST['url'])){
             $dato = $_POST['dato'];
             $url = $_POST['url'];
 
             $hoy = date("Y-m-d H:i:s");
 
-            //conectarnos a la BD
-            $host = "localhost";
-            $database = "l1_pweb";
-            $user = "root";
-            $pass = "";
-
-            $connection = mysqli_connect($host,$user,$pass, $database);
-
-            //comprobar error
-            if(mysqli_connect_errno()){
-                echo "ERROR";
-                die(mysqli_connect_error()); //die == exit)
+            $servername = "localhost";
+            $username = "root";
+            $password = "";
+            $bdname = "laboratorio";
+            
+            $conn = mysqli_connect($servername, $username, $password, $bdname);
+            // Check connection
+            
+            if (!$conn) {
+                  die("Connection failed: " . mysqli_connect_error());
             }
+             
+            echo "Connected successfully";
 
             //petición url para el canal
             $sql = "SELECT * from canales WHERE url = '$url'";
 
             echo $sql;
 
-            if($result = mysqli_query($connection, $sql)){
+            if($result = mysqli_query($conn, $sql)){
                 if(mysqli_num_rows($result)){
                     $row = mysqli_fetch_assoc($result);
                     $idCanal = $row['id'];
@@ -33,17 +33,17 @@
                     echo "Canal inválido";
                 }
             }else{
-                echo mysqli_error($connection);
+                echo mysqli_error($conn);
             }
 
             //petición sql para el sensor
             $sql = "INSERT INTO datossensores (id_canal, dato, fecha)
             VALUES ('$idCanal', '$dato', '$hoy')";
 
-            if(mysqli_query($connection, $sql)){
+            if(mysqli_query($conn, $sql)){
                 echo "Correcto";
             }else{
-                echo mysqli_error($connection);
+                echo mysqli_error($conn);
             }
 
 
