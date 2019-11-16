5bb6b472940a869bc0803ea0bc35e10607987ee4
ErikMartrus <erik_mg94@hotmail.es>
2019-11-16 12:36:28 +0000

    servicioWeb.php

---
+++ public/servicioWeb.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="description" content="This is an HTML5/CSS3 example">
+  <meta name="keywords" content="HTML5, CSS3, JavaScript">
+  <title>Servicio Web para IoT</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/misEstilos.css">
+
+  <body>
+  <!-- 10% -->
+  <header>
+    <nav class="cabecera navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="index.php"><img src="https://picsum.photos/120/38" alt="" style="object-fit: cover"></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent"
+        style="display: flex; justify-content:space-between">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="index.php">MywebIOT <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="canales.php">Canales</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="ayuda.php">Ayuda</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="contacto.php">Contactos</a>
+          </li>
+        </ul>
+    <?php
+    if(isset($_SESSION["user"])){
+      $nombreUsuario = $_SESSION["user"]["nombre"];
+
+    ?> 
+          <ul class="navbar-nav">
+          <li class="nav-item active">
+            <a class="nav-link" href="FormularioLogin.php"><?php echo $nombreUsuario?><span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" href="../php/LogOut.php">LogOut<span class="sr-only">(current)</span></a>
+          </li>
+        </ul>
+        <?php
+          }else{
+        ?>
+
+        <ul class="navbar-nav">
+          <li class="nav-item active">
+            <a class="nav-link" href="FormularioLogin.php">Login <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="register.php">Register</a>
+          </li>
+
+        </ul>
+
+      
+      <?php
+      }
+      ?>
+      </div>
+    </nav>
+  </header>
+
+
+  <form action="resultServicioWeb.php" method="POST">
+    <div class="contenedor--form">
+      <div class="items--form">
+        <label>Desde: </label>
+        <input type="date" name="fechaDesde" id="fechaDesde"/>
+      </div>
+
+      <div class="items--form">
+        <label>Hasta: </label>
+        <input type="date" name="fechaHasta" id="fechaHasta"/>
+      </div>
+    </div>
+    <div class="contenedor--btn">
+
+      <div class="items-grid">
+        <div class="enter">
+        <input type="reset" name="cancel" value="Cancelar" />
+        <input type="submit" name="search" value="Buscar"/>
+        </div>
+      </div>
+    </div>
+
+  </form>
+
+  <footer id="piePaginaLogin">
+    <p class="text">Footer</p>
+  </footer>
+</body>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+  integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+  integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+  integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+</html>
