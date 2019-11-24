73b21b91eb89be282d6d966570e3bd0b11e772ef
ErikMartrus <erik_mg94@hotmail.es>
2019-11-24 19:58:51 +0000

    cambiando scripts de jquery en todos los archivos

--- public/misCanales.php
+++ public/misCanales.php
@@ -24,13 +24,16 @@ if (!$conn) {
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
     integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   <link rel="stylesheet" href="css/misEstilos.css">
-  <script> function borrar(dato){
+
+  <script src="https://code.jquery.com/jquery-3.4.1.js"></script>
+  <script> 
+    function borrar(dato){
         alert(dato);
         $.post("php/borrar_canal.php", {id: dato}, location.reload());
         
     }
     
-    </script>
+  </script>
 </head>
 
 <body>
