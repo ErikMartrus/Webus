a256bc5966bb56359532d26be63b0fd7c0dd7b77
ErikMartrus <erik_mg94@hotmail.es>
2019-12-12 10:23:10 +0000

    ajustes en estilo.php

--- public/articulosCompra.php
+++ public/articulosCompra.php
@@ -1,15 +1,20 @@
 <?php
     if (isset($_POST['idproducto'])) {
         if  (isset($_POST['cantidad'])) {
+
+            echo 'paso 1';
             //Añadir producto al carrito
             if (isset($_SESSION['carrito'][$_POST['idproducto']]))
                 $_SESSION['carrito'][$_POST['idproducto']] += $_POST['cantidad'];
             else
                 $_SESSION['carrito'][$_POST['idproducto']] = $_POST['cantidad'];
+                echo 'paso 2';
         }
         elseif (isset ($_POST['Eliminar'])) {
-            //Eliminar producto del carrito
-            unset ($_SESSION['carrito']['idProducto']);
+            echo 'paso 3';
+            //Eliminar producto del carrito           
+            unset ($_SESSION['carrito'][$_POST['idproducto']]);
+           
         }
     }
     elseif (isset($_POST['Vaciar'])) {
@@ -26,10 +31,14 @@
         echo "El carrito tiene ".count($_SESSION['carrito'])." productos<br>";
         echo "<table><tr><th>Cantidad</th></tr>";
         foreach ($_SESSION['carrito'] as $id => $valor) {
-            echo "<tr><td>$valor</td>";
-            echo "<td><form method='post' action='carrito.php'>";
-            echo "<input type='submit' name='Eliminar' value='Eliminar'>";
-            echo "</form></td></tr>";
+            echo "
+            <tr><td>$valor</td>
+            <td>
+            <form method='post' action='carrito.php'>
+                <input type='submit' name='Eliminar' value='Eliminar'>
+            </form>
+            </td></tr>
+            ";
         }
         echo "</table>";
 
