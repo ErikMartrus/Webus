f00e30ad8e3e8498424cb97e1fa7825b38e580cd
ErikMartrus <erik_mg94@hotmail.es>
2020-01-08 17:41:19 +0000

    cambios en las href para establecer bien las direcciones

--- public/social/miembros.php
+++ public/social/miembros.php
@@ -12,7 +12,7 @@
   <title>Miembros de la web IOT</title>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
     integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-  <link rel="stylesheet" href="css/misEstilos.css">
+  <link rel="stylesheet" href="../css/misEstilos.css">
   
   <script src="https://code.jquery.com/jquery-3.4.1.js"></script>
 </head>
@@ -21,7 +21,7 @@
   <!-- 10% -->
   <header>
     <nav class="cabecera navbar navbar-expand-lg navbar-light bg-light">
-      <a class="navbar-brand" href="../index.php"><img class="img-Index" src="assets/img/IOT.png" alt="" style="object-fit: cover"></a>
+      <a class="navbar-brand" href="../index.php"><img class="img-Index" src="../assets/img/IOT.png" alt="" style="object-fit: cover"></a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
         aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -33,22 +33,22 @@
         style="display: flex; justify-content:space-between">
         <ul class="navbar-nav mr-auto">
           <li class="nav-item active">
-            <a class="nav-link" href="index.php">MywebIOT <span class="sr-only">(current)</span></a>
+            <a class="nav-link" href="../index.php">MywebIOT <span class="sr-only">(current)</span></a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="canales.php">Canales</a>
+            <a class="nav-link" href="../canales.php">Canales</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="ayuda.php">Ayuda</a>
+            <a class="nav-link" href="../ayuda.php">Ayuda</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="contacto.php">Contactos</a>
+            <a class="nav-link" href="../contacto.php">Contactos</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="productos.php">Productos</a>
+            <a class="nav-link" href="../productos.php">Productos</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="carrito.php">MyIOT Shop</a>
+            <a class="nav-link" href="../carrito.php">MyIOT Shop</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="social.php">MyIOT Social</a>
@@ -56,10 +56,10 @@
         </ul>
         <ul class="navbar-nav">
           <li class="nav-item active">
-            <a class="nav-link" href="FormularioLogin.php">Login <span class="sr-only">(current)</span></a>
+            <a class="nav-link" href="../FormularioLogin.php">Login <span class="sr-only">(current)</span></a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="register.php">Register</a>
+            <a class="nav-link" href="../register.php">Register</a>
           </li>
 
         </ul>
@@ -74,22 +74,22 @@
         style="display: flex; justify-content:space-between">
         <ul class="navbar-nav mr-auto">
           <li class="nav-item active">
-            <a class="nav-link" href="index.php">MywebIOT <span class="sr-only">(current)</span></a>
+            <a class="nav-link" href="../index.php">MywebIOT <span class="sr-only">(current)</span></a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="canales.php">Canales</a>
+            <a class="nav-link" href="../canales.php">Canales</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="ayuda.php">Ayuda</a>
+            <a class="nav-link" href="../ayuda.php">Ayuda</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="contacto.php">Contactos</a>
+            <a class="nav-link" href="../contacto.php">Contactos</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="productos.php">Productos</a>
+            <a class="nav-link" href="../productos.php">Productos</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="carrito.php">MyIOT Shop</a>
+            <a class="nav-link" href="../carrito.php">MyIOT Shop</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="social.php">MyIOT Social</a>
@@ -104,19 +104,19 @@
         style="display: flex; justify-content:space-between">
         <ul class="navbar-nav mr-auto">
           <li class="nav-item active">
-            <a class="nav-link" href="index.php">MywebIOT <span class="sr-only">(current)</span></a>
+            <a class="nav-link" href="../index.php">MywebIOT <span class="sr-only">(current)</span></a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="canales.php">Canales</a>
+            <a class="nav-link" href="../canales.php">Canales</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="ayuda.php">Ayuda</a>
+            <a class="nav-link" href="../ayuda.php">Ayuda</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="contacto.php">Contactos</a>
+            <a class="nav-link" href="../contacto.php">Contactos</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="carrito.php">MyIOT Shop</a>
+            <a class="nav-link" href="../carrito.php">MyIOT Shop</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="social.php">MyIOT Social</a>
@@ -129,10 +129,10 @@
         ?>
           <ul class="navbar-nav">
           <li class="nav-item active">
-            <a class="nav-link" href="FormularioLogin.php"><?php echo $nombreUsuario?><span class="sr-only">(current)</span></a>
+            <a class="nav-link" href="../FormularioLogin.php"><?php echo $nombreUsuario?><span class="sr-only">(current)</span></a>
           </li>
           <li class="nav-item active">
-            <a class="nav-link" href="php/LogOut.php">LogOut<span class="sr-only">(current)</span></a>
+            <a class="nav-link" href="../php/LogOut.php">LogOut<span class="sr-only">(current)</span></a>
           </li>
           </ul>
         <?php
@@ -140,10 +140,10 @@
         ?>
         <ul class="navbar-nav">
           <li class="nav-item active">
-            <a class="nav-link" href="FormularioLogin.php">Login <span class="sr-only">(current)</span></a>
+            <a class="nav-link" href="../FormularioLogin.php">Login <span class="sr-only">(current)</span></a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="register.php">Register</a>
+            <a class="nav-link" href="../ksregister.php">Register</a>
           </li>
 
         </ul>
--- public/social/social.php
+++ public/social/social.php
@@ -31,7 +31,7 @@ if (!$conn) {
 
   <header>
     <nav class="cabecera navbar navbar-expand-lg navbar-light bg-light">
-      <a class="navbar-brand" href="../index.php"><img class="img-Index" src="assets/img/IOT.png" alt="" style="object-fit: cover"></a>
+      <a class="navbar-brand" href="../index.php"><img class="img-Index" src="../assets/img/IOT.png" alt="" style="object-fit: cover"></a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
         aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
