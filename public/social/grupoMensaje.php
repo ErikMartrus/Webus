fe7e5d015ed7aedac932906a5a01e44f89db5248
ErikMartrus <erik_mg94@hotmail.es>
2020-01-21 01:14:29 +0000

    añadido  solo un grupo o duplicado

--- public/social/grupoMensaje.php
+++ public/social/grupoMensaje.php
@@ -17,14 +17,29 @@ $idUsuario=$_SESSION["user"]["id"];
 $sqlAmigos = "SELECT * FROM grupos WHERE id_admin='$idUsuario'";
 $result2 = mysqli_query($connection, $sqlAmigos);
 if ($result2) {
+    $contador = 0;
+    $nombreAnterior = "";
+
+
     while ($row2 = mysqli_fetch_array($result2)) {
+
         $idPersona = $row2['id_usuario'];
         $nombreGrupo=  $row2['nombre_grupo'];
+
+        if ($contador == 0) {
+            $nombreAnterior = "$nombreGrupo";
+            $contador++;
+            echo "<option value='$idGrupo'>Grupo: $nombreGrupo</option>";
+        } else {
+            if ($nombreGrupo != $nombreAnterior) {
+                $nombreAnterior = $nombreGrupo;
+                echo "<option value='$idGrupo'>Grupo: $nombreGrupo</option>";
+            }
+        }
         
         $idGrupo=  $row2['id'];
        
 
-        echo "<option value='$idGrupo'>Grupo: $nombreGrupo</option>";
     }
 }
     
